7f0570779c7ab0ebdc557e00b4b0f4c2af24d653
devireddyprasanth22 <dpkrdrkr@gmail.com>
2025-03-23 15:31:31 +0800

    Update submodule-2 content

--- build/CMakeFiles/submodule2.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for submodule2.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/dp/Desktop/speqtral_challenge/master-software/submodule-2
-# Build directory: /Users/dp/Desktop/speqtral_challenge/master-software/submodule-2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(submodule2 "/Users/dp/Desktop/speqtral_challenge/master-software/submodule-2/build/test-submodule2")
-set_tests_properties(submodule2 PROPERTIES  _BACKTRACE_TRIPLES "/Users/dp/Desktop/speqtral_challenge/master-software/submodule-2/CMakeLists.txt;18;add_test;/Users/dp/Desktop/speqtral_challenge/master-software/submodule-2/CMakeLists.txt;0;")
